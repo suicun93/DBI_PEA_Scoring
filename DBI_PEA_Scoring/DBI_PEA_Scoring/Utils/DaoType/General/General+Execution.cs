2a0af862b5f2073e79e317da498a27d1fef5b47e
BlaCkMeloDy <mkbvnh@gmail.com>
2019-03-11 08:01:08 +0700

    Chạy ổn ổn rồi, còn vấn đề khi tạo db ở câu 1

--- DBI_PEA_Scoring/DBI_PEA_Scoring/Utils/DaoType/General/General+Execution.cs
+++ DBI_PEA_Scoring/DBI_PEA_Scoring/Utils/DaoType/General/General+Execution.cs
@@ -3,6 +3,8 @@
 using Microsoft.SqlServer.Management.Smo;
 //Microsoft.SqlServer.ConnectionInfo.dll    
 using Microsoft.SqlServer.Management.Common;
+using System;
+using System.Threading;
 
 namespace DBI_PEA_Scoring.Utils.DaoType
 {
@@ -14,27 +16,20 @@ namespace DBI_PEA_Scoring.Utils.DaoType
         /// <param name="query">Query to execute</param>
         public static void ExecuteQuery(string query, string catalog)
         {
-            query = "Use " + "[" + catalog + "];\nGO\n" + query + "";
+            query = "Use " + "[" + catalog + "]\nGO\n" + query + "";
+            string[] queryList = query.Split(new string[] { "GO", "go", "Go", "oG" }, StringSplitOptions.None);
             var builder = Common.Constant.SqlConnectionStringBuilder;
             using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
             {
                 connection.Open();
-                var server = new Server(new ServerConnection(connection));
-                server.ConnectionContext.StatementTimeout = Common.Constant.TimeOutInSecond;
-                server.ConnectionContext.Connect();
-                try
+                for (int i = 0; i < queryList.Length; i++)
                 {
-                    server.ConnectionContext.ExecuteNonQuery(query);
+                    using (SqlCommand command = new SqlCommand(queryList[i], connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
                 }
-                catch (System.Exception)
-                {
-                    throw;
-                }
-                finally
-                {
-                    server.ConnectionContext.ExecuteNonQuery("Use master");
-                    server.ConnectionContext.Disconnect();
-                }
+
             }
         }
 
