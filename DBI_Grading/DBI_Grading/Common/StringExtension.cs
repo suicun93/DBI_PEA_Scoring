537c5c904cc70e39e5e291c8d4f56f9469e8eb63
suicun93 <duchoang191@gmail.com>
2019-03-28 11:31:54 +0700

    Add hamming distance function to evaluate distance from 2 seperate string

--- DBI_Grading/DBI_Grading/Common/StringExtension.cs
+++ DBI_Grading/DBI_Grading/Common/StringExtension.cs
@@ -8,5 +8,19 @@ namespace DBI_Grading.Common
     public static class StringExtension
     {
         internal static string GetNumbers(string input) => new string(input.Where(c => char.IsDigit(c)).ToArray());
+        internal static int GetHammingDistance(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                throw new Exception("Strings must be equal length");
+            }
+
+            int distance =
+                s.ToCharArray()
+                .Zip(t.ToCharArray(), (c1, c2) => new { c1, c2 })
+                .Count(m => m.c1 != m.c2);
+
+            return distance;
+        }
     }
 }
