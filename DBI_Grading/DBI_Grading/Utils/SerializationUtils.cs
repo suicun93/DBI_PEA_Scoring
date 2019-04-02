3a6560346c316e155e5dad9155401a146d7987d5
BlaCkMeloDy <mkbvnh@gmail.com>
2019-04-02 13:11:26 +0700

    refactor

--- DBI_Grading/DBI_Grading/Utils/SerializationUtils.cs
+++ DBI_Grading/DBI_Grading/Utils/SerializationUtils.cs
@@ -24,11 +24,11 @@ namespace DBI_Grading.Utils
         {
             Type returntype;
             // Paper Set
-            if (typeName.StartsWith("DBI_Exam_Creator_Tool.Entities.PaperSet"))
+            if (typeName.StartsWith("DBI202_Creator.Entities.Paper.PaperSet"))
                 returntype = typeof(PaperSet);
             // List<Paper>
             else if (typeName.StartsWith(
-                "System.Collections.Generic.List`1[[DBI_Exam_Creator_Tool.Entities.Paper, DBI_Exam_Creator_Tool, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null]]")
+                "System.Collections.Generic.List`1[[DBI202_Creator.Entities.Paper.Paper, DBI202_Creator, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null]]")
             )
                 returntype = typeof(List<Paper>);
             // List<String>
@@ -42,29 +42,29 @@ namespace DBI_Grading.Utils
             )
                 returntype = typeof(List<int>);
             // QuestionSet
-            else if (typeName.StartsWith("DBI_Exam_Creator_Tool.Entities.QuestionSet"))
+            else if (typeName.StartsWith("DBI202_Creator.Entities.Question.QuestionSet"))
                 returntype = typeof(QuestionSet);
             // Paper
-            else if (typeName.StartsWith("DBI_Exam_Creator_Tool.Entities.Paper"))
+            else if (typeName.StartsWith("DBI202_Creator.Entities.Paper.Paper"))
                 returntype = typeof(Paper);
             // List<Question>
             else if (typeName.StartsWith(
-                "System.Collections.Generic.List`1[[DBI_Exam_Creator_Tool.Entities.Question, DBI_Exam_Creator_Tool, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null]]")
+                "System.Collections.Generic.List`1[[DBI202_Creator.Entities.Question.Question, DBI202_Creator, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null]]")
             )
                 returntype = typeof(List<Question>);
             // Question
-            else if (typeName.StartsWith("DBI_Exam_Creator_Tool.Entities.Question"))
+            else if (typeName.StartsWith("DBI202_Creator.Entities.Question.Question"))
                 returntype = typeof(Question);
             // List<Candidate>
             else if (typeName.StartsWith(
-                "System.Collections.Generic.List`1[[DBI_Exam_Creator_Tool.Entities.Candidate, DBI_Exam_Creator_Tool, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null]]")
+                "System.Collections.Generic.List`1[[DBI202_Creator.Entities.Candidate.Candidate, DBI202_Creator, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null]]")
             )
                 returntype = typeof(List<Candidate>);
             // Candidate.QuestionTypes
-            else if (typeName.StartsWith("DBI_Exam_Creator_Tool.Entities.Candidate+QuestionTypes"))
+            else if (typeName.StartsWith("DBI202_Creator.Entities.Candidate.Candidate+QuestionTypes"))
                 returntype = typeof(Candidate.QuestionTypes);
             // Candidate
-            else if (typeName.StartsWith("DBI_Exam_Creator_Tool.Entities.Candidate"))
+            else if (typeName.StartsWith("DBI202_Creator.Entities.Candidate.Candidate"))
                 returntype = typeof(Candidate);
             else
                 returntype = Type.GetType(string.Format("{0}, {1}", typeName, assemblyName));
